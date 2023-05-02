3dcf8d181195a3721c5ead39c3971426f8f711dd
Divij Pawar <divijpawar@localhost.localdomain>
2023-05-02 09:00:01 +0530

    initial code

---
+++ memp.py
@@ -0,0 +1,17 @@
+import sysv_ipc as ipc
+
+def main():
+    path = "/tmp"
+    key = ipc.ftok(path, 2333)
+    shm = ipc.SharedMemory(key, 0, 0)
+
+    #I found if we do not attach ourselves
+    #it will attach as ReadOnly.
+    shm.attach(0,0)
+    buf = shm.read(19)
+    print(buf)
+    shm.detach()
+    pass
+
+if __name__ == '__main__':
+    main()
